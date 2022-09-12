d55a74919f33edd2ac1aee3b30229a37c96423b8
yakc <yakc@users.noreply.github.com>
2022-09-11 17:58:24 -0700

    support RFC-2047 for non-ASCII text
    Also bump std to 0.155 to match version in rfc2047

---
+++ lib/mid/rfc2047.ts
@@ -0,0 +1,18 @@
+import { decode } from "rfc2047";
+import { From } from "yooznooz/lib/usenet.ts";
+import { ExtArticle, ExtOverview } from "yooznooz/lib/ware.ts";
+
+function decodeFrom(from: From) {
+  from.name = decode(from.name);
+}
+
+export default {
+  overview: (o: ExtOverview) => {
+    decodeFrom(o.from);
+    o.subject = decode(o.subject);
+  },
+  article: (a: ExtArticle) => {
+    decodeFrom(a.from);
+    a.subject = decode(a.subject);
+  },
+};
--- lib/proc_wrap.ts
+++ lib/proc_wrap.ts
@@ -7,8 +7,8 @@ register(multipart);
 import { default as windows1252 } from "./mid/windows1252.ts";
 register(windows1252);
 
-// import { default as rfc2047 } from "./mid/rfc2047.js";
-// register(rfc2047);
+import { default as rfc2047 } from "./mid/rfc2047.ts";
+register(rfc2047);
 
 import { default as signature } from "./mid/signature.ts";
 register(signature);
